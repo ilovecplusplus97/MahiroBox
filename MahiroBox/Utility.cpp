3c69b0b19ce4c1feae0bb6c550d095e6ab22e7ea
ilovecplusplus97 <2383130098@qq.com>
2023-01-08 14:40:00 +0800

    修复了设置界面按键名称显示错误的bug

--- MahiroBox/Utility.cpp
+++ MahiroBox/Utility.cpp
@@ -3,7 +3,7 @@
 
 std::wstring get_scancode_name(DWORD scancode) {
     WCHAR keyname[16];
-    if (GetKeyNameText((LONG)scancode << 16L + (1L << 25L), keyname, 16) == 0) {
+    if (GetKeyNameTextW((LONG)scancode << 16L, keyname, 16) == 0) {
         return std::format(L"scancode: {}", scancode);
     }
     return keyname;
